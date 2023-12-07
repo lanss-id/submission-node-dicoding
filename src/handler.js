b0dd2600bff9e12dabf8161bc46990bbb6d0839a
Maulana Kayyis <maulanakayyis354@gmail.com>
2023-12-07 10:57:32 +0700

    fix(detail book): fixing detail book api

--- src/handler.js
+++ src/handler.js
@@ -2,118 +2,97 @@ const { nanoid } = require('nanoid')
 const bookshelf = require('./bookshelf')
 
 const addBooksHandler = (request, h) => {
-  try {
-    const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
 
-    if (!name || name === '') {
-      return h.response({
-        status: 'fail',
-        message: 'Gagal menambahkan buku. Mohon isi nama buku'
-      }).code(400)
-    }
+  if (!name || name === '') {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku'
+    }).code(400)
+  }
 
-    if (readPage > pageCount) {
-      return h.response({
-        status: 'fail',
-        message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
-      }).code(400)
-    }
+  if (readPage > pageCount) {
+    return h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount'
+    }).code(400)
+  }
 
-    const id = nanoid(16)
-    const createdAt = new Date().toISOString()
-    const finished = pageCount === readPage
+  const id = nanoid(16)
+  const createdAt = new Date().toISOString()
+  const finished = pageCount === readPage
 
-    const newBook = {
-      id,
-      name,
-      year,
-      author,
-      summary,
-      publisher,
-      pageCount,
-      readPage,
-      finished,
-      reading,
-      createdAt,
-      updatedAt: createdAt
-    }
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    createdAt,
+    updatedAt: createdAt
+  }
 
-    bookshelf.push(newBook)
+  bookshelf.push(newBook)
 
-    const isSuccess = bookshelf.filter((book) => book.id === id).length > 0
+  const isSuccess = bookshelf.filter((book) => book.id === id).length > 0
 
-    if (isSuccess) {
-      return h.response({
-        status: 'success',
-        message: 'Buku berhasil ditambahkan',
-        data: {
-          bookId: id
-        }
-      }).code(201)
-    }
-  } catch (error) {
-    if (!error.response) throw error
-    if (error.response.statusCode === 400) {
-      return h.response({ message: 'bad request' }).code(400)
-    }
-    if (error.response.statusCode === 404) {
-      return h.response({ message: 'data tidak ditemukan' }).code(404)
-    }
-    throw error
+  if (isSuccess) {
+    return h.response({
+      status: 'success',
+      message: 'Buku berhasil ditambahkan',
+      data: {
+        bookId: id
+      }
+    }).code(201)
   }
 }
 
 const getAllBooksHandler = () => {
-  try {
-    const book = bookshelf.map(book => ({
-      id: book.id,
-      name: book.name,
-      publisher: book.publisher
-    }))
+  const book = bookshelf.map(book => ({
+    id: book.id,
+    name: book.name,
+    publisher: book.publisher
+  }))
+
+  if (!book) {
+    return h.response({ message: 'data tidak ditemukan' }).code(404)
+  }
   
-    return {
-      status: 'success',
-      data: {
-        book
-      }
+  return {
+    status: 'success',
+    data: {
+      book
     }
-  } catch (error) {
-    if (!error.response) throw error
-    if (error.response.statusCode === 400) {
-      return h.response({ message: 'bad request' }).code(400)
-    }
-    if (error.response.statusCode === 404) {
-      return h.response({ message: 'data tidak ditemukan' }).code(404)
-    }
-    throw error
   }
 }
 
 const detailBookHandler = (request, h) => {
-  try {
-    const { bookId } = request.params
-    const {detailBook : book} = bookshelf.find((book) => book.id === bookId)
-    if (book) {
-      const response = h.response({
-        status: 'success',
-        message: 'Detail Buku berhasil didapatkan',
-        data: {
-          book
-        }
-      })
-      response.code(200)
-      return response
+  const { bookId } = request.params;
+  const book = bookshelf.find((book) => book.id === bookId);
+
+  if (!book) {
+    return h.response({ status: 'fail', message: 'Buku tidak ditemukan' }).code(404);
+  }  
+
+  const response = h.response({
+    status: 'success',
+    message: 'Detail Buku berhasil didapatkan',
+    data: {
+      book
     }
-  } catch (error) {
-    if (!error.response) throw error
-    if (error.response.statusCode === 400) {
-      return h.response({ message: 'bad request' }).code(400)
-    }
-    if (error.response.statusCode === 404) {
-      return h.response({ status: 'fail', message: 'Buku tidak ditemukan' }).code(404)
-    }
-    throw error
-  }
+  });
+  response.code(200);
+  return response;
 }
 
-module.exports = { addBooksHandler, getAllBooksHandler, detailBookHandler }
+const updateBookHandler = (request, h) => {
+  const {bookId} = request.params
+  
+}
+
+module.exports = { addBooksHandler, getAllBooksHandler, detailBookHandler, updateBookHandler }
